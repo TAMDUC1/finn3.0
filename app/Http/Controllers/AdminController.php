560c3df116bef08b564440a4f1fca9c825283ce7
TamDuc <tamduc@stud.ntnu.no>
2018-08-14 10:46:26 +0700

    initial commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function login(Request $request){
+
+        if($request->isMethod('post')){
+            $data = $request->input();
+             if(Auth::attempt(['email'  =>$data['email'],'password'  => $data['password'],'admin' => '1'])){
+                 return view('admin.dashboard');
+             } else{
+                 return back()->with('flash-message', 'Message!');
+
+             }
+             return view('admin.login');
+        }
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//-----------------------------------------
+    // Controllers Within The "App\Http\Controllers\Admin" Namespace
+    Route::resource('admin','AdminController');
+    Route::prefix('admin')->group(function () {
+        Route::get('/', function () {
+            return view('admin.login');
+        });
+
+        Route::get('/index', 'AdminController@index')->name('admin.index');
+
+        Route::post('login', 'AdminController@login')->name('adminLogin');
+      //  Route::post()
+    });
+
+
+
+
+//-----------------------------------------
